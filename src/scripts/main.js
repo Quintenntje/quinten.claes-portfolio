6726f231dc5cc7fc993766a33f0fcae0dc4c7e52
Quinten <quintenclaes7@gmail.com>
2025-03-27 19:08:39 +0100

    added popover

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,3 +5,12 @@ import { initModals } from "./modal.js";
 expandNavigationMenu();
 showBlurEffectOnMouse();
 initModals();
+
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData);
+  console.log(data);
+});
